2a3bee9a035a58e0edb5487e71afb304a3f7316e
Saeid Heidari <saeiiid.heidari@gmail.com>
2024-12-04 12:21:58 +0330

    add comment related urls

--- post/urls.py
+++ post/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .apis import BlogPostAPI
+from .apis import BlogPostAPI, CommentAPI
 
 urlpatterns = [
     path(
@@ -12,5 +12,15 @@ urlpatterns = [
         "posts/<int:id>/",
         BlogPostAPI.as_view(),
         name="blogpost-delete",
+    ),
+    path(
+        "posts/<int:post_id>/comments/",
+        CommentAPI.as_view(),
+        name="comment-list",
+    ),
+    path(
+        "posts/<int:post_id>/comments/<int:comment_id>/",
+        CommentAPI.as_view(),
+        name="comment-delete",
     ),
 ]
