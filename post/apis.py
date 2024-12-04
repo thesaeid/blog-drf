779c111bcbb791cefa173cf0aa1ead535aad3d82
Saeid Heidari <saeiiid.heidari@gmail.com>
2024-12-04 12:21:46 +0330

    add comments api

--- post/apis.py
+++ post/apis.py
@@ -1,12 +1,18 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 
-from .models import Tag, BlogPost
-from .serializers import BlogPostSerializer, BlogPostCreateSerializer, TagSerializer
+from .models import Tag, BlogPost, Comment
+from .serializers import (
+    BlogPostSerializer,
+    BlogPostCreateSerializer,
+    TagSerializer,
+    CommentSerializer,
+)
 from .filters import BlogPostFilter
 from .paginations import BlogPostPagination
 
@@ -51,17 +57,17 @@ class BlogPostAPI(APIView):
                 post.tags.set(tag_instances)
                 post.save()
                 return Response(
-                    data="Your Post is Published.",
+                    data={"detail": "Your post is published."},
                     status=status.HTTP_201_CREATED,
                 )
 
             return Response(
-                data="You're not allowed to make a post.",
+                data={"detail": "You're not allowed to make a post."},
                 status=status.HTTP_403_FORBIDDEN,
             )
         except user_model.DoesNotExist:
             return Response(
-                data="You're not allowed to do this.",
+                data={"detail": "You're not allowed to do this."},
                 status=status.HTTP_403_FORBIDDEN,
             )
 
@@ -109,7 +115,7 @@ class BlogPostAPI(APIView):
             )
         except user_model.DoesNotExist:
             return Response(
-                data="You're not allowed to do this.",
+                data={"detail": "You're not allowed to do this."},
                 status=status.HTTP_403_FORBIDDEN,
             )
 
@@ -143,6 +149,40 @@ class BlogPostAPI(APIView):
             )
         except user_model.DoesNotExist:
             return Response(
-                data="You're not allowed to do this.",
+                data={"detail": "You're not allowed to do this."},
                 status=status.HTTP_403_FORBIDDEN,
             )
+
+
+class CommentAPI(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, post_id):
+        post = get_object_or_404(BlogPost, pk=post_id)
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(author=request.user, post=post)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, post_id):
+        post = get_object_or_404(BlogPost, pk=post_id)
+        comments = post.comments.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, post_id, comment_id):
+        comment = get_object_or_404(Comment, pk=comment_id, post__pk=post_id)
+
+        if request.user.role == "admin" or comment.author == request.user:
+            comment.delete()
+            return Response(
+                data={"detail": "comment deleted."},
+                status=status.HTTP_204_NO_CONTENT,
+            )
+
+        return Response(
+            {"detail": "You do not have permission to delete this comment."},
+            status=status.HTTP_403_FORBIDDEN,
+        )
